adf03d2fb072f4e270aa0f29ad6afba82e98e80b
simonhfrost <simonhfrost@gmail.com>
2015-10-04 20:27:35 +0200

    ES2015 hello world with babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp')
 var Browserify = require('browserify')
+var babelify = require('babelify')
 
 var browserify = Browserify()
 browserify.add('main.js')
 
 gulp.task('default', function() {
-  browserify.bundle()
+  browserify.transform(babelify)
+            .bundle()
             .pipe(process.stdout)
 });
--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log( 'hello world' )
+class HelloWorld {
+  constructor() {
+    this.message = 'hello world'
+  }
+
+  print() {
+    console.log(this.message)
+  }
+}
+
+var helloWorld = new HelloWorld()
+helloWorld.print()
