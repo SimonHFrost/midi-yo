cbb4e4f2bc36c5e796369b1c522724098332514d
simonhfrost <simonhfrost@gmail.com>
2015-10-18 15:36:18 +0200

    remove semicolons

--- src/scene.js
+++ src/scene.js
@@ -9,29 +9,29 @@ class Scene {
   }
 
   init() {
-    var geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-    this.mesh = new THREE.Mesh( geometry, material );
+    var geometry = new THREE.BoxGeometry( 200, 200, 200 )
+    var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } )
+    this.mesh = new THREE.Mesh( geometry, material )
 
-    this.scene = new THREE.Scene();
-    this.scene.add( this.mesh );
+    this.scene = new THREE.Scene()
+    this.scene.add( this.mesh )
 
-    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    this.camera.position.z = 1000;
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 )
+    this.camera.position.z = 1000
 
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer = new THREE.WebGLRenderer()
+    this.renderer.setSize( window.innerWidth, window.innerHeight )
 
-    document.body.appendChild( this.renderer.domElement );
+    document.body.appendChild( this.renderer.domElement )
   }
 
   animate() {
-    requestAnimationFrame( this.animate.bind(this) );
+    requestAnimationFrame( this.animate.bind(this) )
 
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.02;
+    this.mesh.rotation.x += 0.01
+    this.mesh.rotation.y += 0.02
 
-    this.renderer.render( this.scene, this.camera );
+    this.renderer.render( this.scene, this.camera )
   }
 }
 
