8c546d1a10d28b8ab20f15e2dfe69ba8b674c557
simonhfrost <simonhfrost@gmail.com>
2015-11-16 20:56:41 +0100

    Demo midi

--- src/midi.js
+++ src/midi.js
@@ -3,15 +3,20 @@ var midi = require( 'web-midi' )
 var inStream = midi.openInput('Launchpad')
 var outStream = midi.openOutput('Launchpad')
 
-outStream.write([176,0,0])
-
 inStream.on( 'data', function( data ){
   console.log( data )
 })
 
-// send on note
-outStream.write([146, 38, 127])
+// Clear
+outStream.write([176,0,0])
 
-setTimeout(function(){
-  outStream.write([146, 38, 0])
-}, 1000)
+sendMidi([1,1])
+sendMidi([2,2])
+sendMidi([3,3])
+sendMidi([4,4])
+
+function sendMidi( position ) {
+  var externalPosition = position[1] * 16 + position[0]
+  console.log( externalPosition )
+  outStream.write([144, externalPosition, 60])
+}
