85c6dab488a69d254c9f7c808cc47564b437865c
simonhfrost <simonhfrost@gmail.com>
2017-03-04 18:42:43 +0100

    Hook up events

--- src/launchpad.js
+++ src/launchpad.js
@@ -1,73 +1,73 @@
 let EventEmitter = require('events').EventEmitter
-let WebMidi = require('web-midi')
+
+var MidiStream = require('midi-stream')
+var MidiController = require('midi-controller')
+
+var duplexStream = MidiStream('Launchpad')
+var launchpad = MidiController(duplexStream)
+
+var colors = {
+  green: 60,
+  red: 13,
+  amber: 63,
+  amberLow: 29
+}
 
 class Launchpad extends EventEmitter {
   constructor () {
     super()
 
-    this.inStream = null
-    this.outStream = null
+    this.bindEvents()
 
-    // this._initStreams()
-    // this.bindEvents()
-
-    // this.clear()
+    this.clear()
   }
 
   clear () {
-    this.outStream.write([176, 0, 0])
-  }
-
-  output (position, value) {
-    if (position[0] > 7 || position[1] > 7) {
-      return // Fail graciously if out of bounds
-    }
-
-    let newValue = value || 0
-    let externalPosition = position[1] * 16 + position[0]
-    this.outStream.write([144, externalPosition, newValue])
+    duplexStream.write([176, 0, 0])
   }
 
   bindEvents () {
-    this.inStream.on('data', (data) => {
-      if (data[2] === 0) { // Ignore the off signal
-        return
+      // map launchpad grid to chromatic midi notes (starting at 30)
+    var mapping = []
+    var offset = 30
+    for (var y = 0; y < 8; y++) {
+      for (var x = 0; x < 8; x++) {
+        var id = (y * 16) + x
+        mapping.push([
+          [144, id],       // input midi message
+          [144, offset++]  // output midi message
+        ])
       }
+    }
 
-      let midiNumber = data[1]
-      let scaled = (midiNumber - 36)
-
-      let y = Math.floor(scaled / 4)
-      let x = scaled - (y * 4)
-      let coordinates = [x, y]
-
-      if (coordinates[1] > 7) {
-        coordinates[0] = coordinates[0] + 4
-        coordinates[1] = coordinates[1] - 8
+    var noteMatrix = launchpad.createNoteMatrix(mapping, colors.amber)
+    noteMatrix.on('data', function (midiNote) {
+      if (midiNote[2]) { // note on
+        noteOn(midiNote[1])
+      } else { // note off
+        noteOff(midiNote[1])
       }
-
-      // Invert coordinates
-      coordinates[1] = 7 - coordinates[1]
-
-      console.log(coordinates)
-
-      // The following is normal mode of launchpad
-
-      // let y = Math.floor(midiNumber / 16)
-      // NOTE: Invert y axis, since launchpad and threejs are opposite
-      // let x = midiNumber - (y * 16)
-      // let coordinates = [x, y]
-
-      this.emit('recieved', coordinates)
     })
-  }
 
-  _initStreams () {
-    try {
-      this.inStream = WebMidi.openInput('Launchpad')
-      this.outStream = WebMidi.openOutput('Launchpad')
-    } catch (error) {
-      window.alert('No launchpad found')
+    noteMatrix.pipe(noteMatrix) // echo the notes back to light up buttons
+
+    // TODO Fix this hack
+    var that = this
+
+    // screen synth
+    var onNotes = {}
+    function noteOn (note) {
+      onNotes[note] = setInterval(function () {
+        console.log(note)
+        that.emit('recieved', [5, note % 5])
+      }, 50)
+    }
+
+    function noteOff (note) {
+      if (onNotes[note]) {
+        clearInterval(onNotes[note])
+        onNotes[note] = null
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 let Scene = require('./scene.js')
 let FlatScene = require('./flat-scene.js')
 let Launchpad = require('./launchpad.js')
-let LaunchpadLibTest = require('./launchpad-lib-test.js')
 
 class Main {
   constructor () {
@@ -15,32 +14,23 @@ class Main {
     this.launchpad = new Launchpad()
 
     this.bindEvents()
-    this._initLights()
+    this.launchpad.clear()
   }
 
   bindEvents () {
-    // this.launchpad.on('recieved', (coordinates) => {
-    //   this.launchpad.output(coordinates, 60)
-    //   let cube = this.scene.createCube(coordinates[0], coordinates[1])
-    //   let square = this.flatScene.createSquare(coordinates[0], coordinates[1])
-    //
-    //   setTimeout(() => {
-    //     this.launchpad.output(coordinates)
-    //     this.scene.removeCube(cube)
-    //     this.flatScene.removeSquare(square)
-    //   }, 100)
-    // })
+    this.launchpad.on('recieved', (coordinates) => {
+      // this.launchpad.output(coordinates, 60)
+      let cube = this.scene.createCube(coordinates[0], coordinates[1])
+      let square = this.flatScene.createSquare(coordinates[0], coordinates[1])
+
+      setTimeout(() => {
+        // this.launchpad.output(coordinates)
+        this.scene.removeCube(cube)
+        this.flatScene.removeSquare(square)
+      }, 100)
+    })
   }
 
-  _initLights () {
-    // for (let i = 0; i < 8; i++) {
-    //   for (let j = 0; j < 8; j++) {
-    //     this.launchpad.output([i, j])
-    //   }
-    // }
-    //
-    // setTimeout(() => { this.launchpad.clear() }, 1000)
-  }
 }
 
 /*eslint-disable*/
